17aca3c9be326f9abdc1d90d6e54baa5c6df44c3
JustinYates <Justin.yates24601@gmail.com>
2018-01-23 16:25:50 -0500

    Latest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+// use this scanner for user input
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Hello What Is Your Name?";
-        System.out.println(name);
-        
+
+        //1. allow user input of a number (guess)
+        //2. tell the user if their guess is too high or too low
+        //3. Allow for 5 chances, if the number is'nt right tell them what it is
+        //4. If the number is right congratulate them
+        //5. Allow the user to play again if they,d like
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your Guess:");
+        int userGuess = input.nextInt();
     }
 }
