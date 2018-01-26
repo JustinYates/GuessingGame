81c297a0985c8a7ff119bb8a6ea39420fbcc3ca0
JustinYates <Justin.yates24601@gmail.com>
2018-01-26 15:28:09 -0500

    newest

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import com.sun.tools.javac.tree.JCTree;
+
 import java.util.Scanner;
+
 // use this scanner for user input
 public class Main {
 
@@ -11,9 +14,44 @@ public class Main {
         //3. Allow for 5 chances, if the number is'nt right tell them what it is
         //4. If the number is right congratulate them
         //5. Allow the user to play again if they,d like
+        // Side Notes
+        // > Is Greater than
+        // < Is less Than
+        // >= Greater than or equal to
+        // <= Less than or equal to
+        // != Not equal to
+        // == Equal to
+        // ! Is not
+        // && And
+        // || is or
+        // 0 is counting as a first chance
+
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Your Guess:");
-        int userGuess = input.nextInt();
+        int numberOfGuesses = 0;
+        while (numberOfGuesses < 5) {
+
+            System.out.println("Enter Your Guess:");
+
+            int userGuess = input.nextInt();
+
+            int rNumber = (int) Math.ceil(Math.random() * 100);
+
+            if (userGuess > rNumber) {
+
+                System.out.println("Your number is too high, Try Again");
+                numberOfGuesses++;
+
+            } else if (userGuess < rNumber) {
+                System.out.println("Your number is too low, try again");
+                numberOfGuesses++;
+            } else if (userGuess == rNumber)
+                System.out.println("You have got it right");
+            else if (numberOfGuesses == 5)
+                System.out.print(rNumber);
+        }
+
+        System.out.println("YOU HAVE FAILED");
     }
+
 }
